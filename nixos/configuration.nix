d2439cfbb2f4af62fa7800f5dabae67f04a0f850
Graydon Cory <graydoncharlescory@gmail.com>
2025-08-10 13:23:41 -0500

    installed neofetch

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,6 +75,7 @@
     tmux
     kitty
     firefox-unwrapped
+    neofetch
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
