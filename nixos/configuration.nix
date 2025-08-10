f34e24552192eef337627d2febf060d0632486c7
Graydon Cory <graydoncharlescory@gmail.com>
2025-08-10 11:50:56 -0500

    got a working version of hyprland up and running, I forgot to git add

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,15 +63,18 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.hyprland.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    stow
     evil-helix
     fish
     git
+    gh
     stow
     tmux
+    kitty
+    firefox-unwrapped
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
