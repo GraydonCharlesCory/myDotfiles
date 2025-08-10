f0619b0628a0478468ce5dcc464de2ce33f4b628
Graydon Cory <graydoncharlescory@gmail.com>
2025-08-10 17:54:45 -0500

    made yazi open with helix and installed vivaldi

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -77,6 +77,7 @@
     firefox-unwrapped
     neofetch
     yazi
+    vivaldi
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
